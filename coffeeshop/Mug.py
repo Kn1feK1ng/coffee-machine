2e52867bd2f33f56f383a977fa5f6cb095e54d3a
Kn1feK1ng <keanuhvh@gmail.com>
2024-03-10 15:00:39 +1000

    adding mug

---
+++ coffeeshop/Mug.py
@@ -0,0 +1,15 @@
+class Mug:
+
+
+    def __init__(self):
+    
+        self.capacity = 0.3   # mL
+        self.state = 0.1   # clean
+
+
+    def empty(self):
+
+        self.capacity -= 0.1
+
+
+
